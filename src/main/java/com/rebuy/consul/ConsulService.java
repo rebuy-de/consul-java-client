ae77f89a864fb4aa05cfbccd88e49e5b11bb8296
Sven Walter <s.walter@rebuy.de>
2015-12-15 11:03:31 +0100

    Add logger
    Closes #9

--- src/main/java/com/rebuy/consul/ConsulService.java
+++ src/main/java/com/rebuy/consul/ConsulService.java
@@ -2,9 +2,13 @@ package com.rebuy.consul;
 
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.agent.model.NewService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConsulService
 {
+    private static final Logger logger = LoggerFactory.getLogger(ConsulService.class);
+
     public static ConsulServiceBuilder builder()
     {
         return new ConsulServiceBuilder();
@@ -23,16 +27,19 @@ public class ConsulService
     public void register()
     {
         client.agentServiceRegister(service);
+        logger.info("registered service in consul: {}", service.toString());
     }
 
     public void shutdownHook()
     {
         Runtime.getRuntime().addShutdownHook(
             new Thread(this::unregister));
+        logger.info("activated shutdown hook for service: {}", service.toString());
     }
 
     public void unregister()
     {
         client.agentServiceDeregister(service.getId());
+        logger.info("unregistered service in consul: {}", service.toString());
     }
 }
