066c2dfd7fe72a868b57e62ceb2ac8d9211c8b42
Hendrik Pannhorst <h.pannhorst@reBuy.de>
2016-01-07 16:11:26 +0100

    add service discovery to consul service

--- src/main/java/com/rebuy/consul/ConsulService.java
+++ src/main/java/com/rebuy/consul/ConsulService.java
@@ -1,9 +1,15 @@
 package com.rebuy.consul;
 
 import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
 import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.catalog.model.CatalogService;
+import com.rebuy.consul.exceptions.NoServiceFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ConsulService
 {
@@ -41,5 +47,18 @@ public class ConsulService
     {
         client.agentServiceDeregister(service.getId());
         logger.info("unregistered service in consul: {}", service.toString());
+    }
+
+    public CatalogService getRandomService(String serviceName)
+    {
+        List<CatalogService> services = client.getCatalogService(serviceName, QueryParams.DEFAULT).getValue();
+
+        if (services.size() == 0) {
+            throw new NoServiceFoundException(serviceName);
+        }
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        return services.get(random.nextInt(services.size()));
     }
 }
--- src/test/java/com/rebuy/consul/ConsulServiceTest.java
+++ src/test/java/com/rebuy/consul/ConsulServiceTest.java
@@ -1,7 +1,11 @@
 package com.rebuy.consul;
 
 import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
+import com.ecwid.consul.v1.Response;
 import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.catalog.model.CatalogService;
+import com.rebuy.consul.exceptions.NoServiceFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +13,9 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ConsulService.class, ConsulClient.class, NewService.class})
@@ -45,5 +52,42 @@ public class ConsulServiceTest
 
         service.unregister();
         Mockito.verify(clientMock).agentServiceDeregister("42");
+    }
+
+    @Test(expected = NoServiceFoundException.class)
+    public void findService_should_throw_exception_if_no_services_are_found()
+    {
+        Response<List<CatalogService>> response = new Response<>(new ArrayList<>(), 1L, true, 1L);
+        Mockito.when(clientMock.getCatalogService(Mockito.anyString(), Mockito.any(QueryParams.class))).thenReturn(response);
+
+        service.getRandomService("service");
+        Mockito.verify(clientMock).getCatalogService("service", Mockito.any(QueryParams.class));
+    }
+
+    @Test
+    public void findService_should_invoke_client()
+    {
+        List<CatalogService> services = new ArrayList<>();
+        services.add(Mockito.mock(CatalogService.class));
+        Response<List<CatalogService>> response = new Response<>(services, 1L, true, 1L);
+        Mockito.when(clientMock.getCatalogService(Mockito.anyString(), Mockito.any(QueryParams.class))).thenReturn(response);
+
+        service.getRandomService("service");
+        Mockito.verify(clientMock).getCatalogService(Mockito.eq("service"), Mockito.any(QueryParams.class));
+    }
+
+    @Test
+    public void findService_should_return_one_service()
+    {
+        List<CatalogService> services = new ArrayList<>();
+        services.add(Mockito.mock(CatalogService.class));
+        services.add(Mockito.mock(CatalogService.class));
+        services.add(Mockito.mock(CatalogService.class));
+
+        Response<List<CatalogService>> response = new Response<>(services, 1L, true, 1L);
+        Mockito.when(clientMock.getCatalogService(Mockito.anyString(), Mockito.any(QueryParams.class))).thenReturn(response);
+
+        CatalogService catalogService = service.getRandomService("service");
+        assert(services.contains(catalogService));
     }
 }
